fb9f407dd52e78de72ce8b6cd7af53381a0a4fd5
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-07 14:23:59 +0200

    added product app and basic base.html

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('rods', 'Rods'),
+        ('reels', 'Reels'),
+        ('lines', 'Lines'),
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/', blank=True)
+
+    def __str__(self):
+        return self.name
