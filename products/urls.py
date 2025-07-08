4e33d95b26cef17472af6b1a1471af2c7443ad43
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-08 15:06:30 +0200

    updated base.html with buttons to all categories

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('', views.home, name='home'),
+    path('products/', views.all_products, name='all_products'),
+    path('products/<str:category>/', views.category_view, name='category_view'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
