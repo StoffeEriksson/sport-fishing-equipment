ac86a460a2e46a8684897e70d48c8538f0b23b1d
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-17 22:56:13 +0200

    added order_detail to profile view and template. Added simple JS to hide and show order history

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile_view, name='user_profile'),
+    path('orders/', views.order_history, name='order_history'),
+    path('orders/<order_number>/', views.order_detail, name='order_detail'),
 ]
