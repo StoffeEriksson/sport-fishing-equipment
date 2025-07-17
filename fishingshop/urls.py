3ab36906fed24a0ba72e0b0b49f454febd5b69e1
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-17 12:50:46 +0200

    created views, forms and a template for profile app

--- fishingshop/urls.py
+++ fishingshop/urls.py
@@ -19,12 +19,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
 
 if settings.DEBUG:
