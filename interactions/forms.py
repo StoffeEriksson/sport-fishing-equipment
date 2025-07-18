300d8161e321c2eab27eee829deef02c18ea71a3
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-18 22:27:26 +0200

    created view and form in interactions app

---
+++ interactions/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Rating, Comment
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['score']
+        widgets = {
+            'score': forms.RadioSelect(choices=[(i, f'{i} ⭐') for i in range(1, 6)])
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment...'})
+        }
