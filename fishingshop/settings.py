50a8651f6686f2fd3e2eb6671331de4c5075ac18
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-20 13:51:04 +0200

    fixed static_root

--- fishingshop/settings.py
+++ fishingshop/settings.py
@@ -165,8 +165,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -179,7 +181,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'sportfishingequipmentbucket '
+    AWS_STORAGE_BUCKET_NAME = 'sportfishingequipmentbucket'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
