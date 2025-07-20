c782749c7bce86fa9be34f165732cd84c341515a
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-07-20 12:45:31 +0200

    moved custom_storages.py to main root

--- custom_storages.py
+++ custom_storages.py
@@ -4,10 +4,7 @@ from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    default_acl = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    file_overwrite = False
-    default_acl = 'public-read'
--- fishingshop/settings.py
+++ fishingshop/settings.py
@@ -182,8 +182,8 @@ if 'USE_AWS' in os.environ:
     STATICFILES_LOCATION = 'static'
     MEDIAFILES_LOCATION = 'media'
 
-    STATICFILES_STORAGE = 'aws_storage.custom_storages.StaticStorage'
-    DEFAULT_FILE_STORAGE = 'aws_storage.custom_storages.MediaStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
